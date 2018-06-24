a21fa51e5a0eb4bd1c1d3ab3a6952af4ad28a47c
lightcreaater <lightkheehing@gmail.com>
2018-06-24 21:16:18 +0800

    working
    pushing cant find the .val() but can push
    pulling works just as fine

--- app.js
+++ app.js
@@ -156,9 +156,7 @@ app.use('/adventure',adventureRouter);
 app.use('/sciencefiction',sciencefictionRouter);
 app.get('/about', comments.list);
 app.delete('/about/:comments_id',comments.delete);
-
 app.use('/edit',editRouter);
-
 app.use('/bank',bankRouter);
 
 //set up routes for images
